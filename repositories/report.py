d31b049253d8850fe88c86810ab21a81b0672353
Risyad Rais <satmo54@gmail.com>
2024-12-21 06:30:08 +0700

    Added group by functions

--- repositories/report.py
+++ repositories/report.py
@@ -6,4 +6,10 @@ class Report(Crud):
     def range_report(self, since, until):
         return self.db.query(
             self.model.juleha_id, func.count(self.model.waktu_sembelih)
-        ).filter(self.model.waktu_sembelih.between(since, until)).all()
+        ).filter(self.model.waktu_sembelih.between(since, until))
+
+    def group_by_juleha(self, query):
+        return query.group_by(self.model.juleha_id)
+
+    def group_by_penyelia(self, query):
+        return query.group_by(self.model.penyelia_id)
